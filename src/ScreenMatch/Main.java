79857ae3b6f0ee62fcadebfef032846a60c8e274
levysampaio <levysampaio@live.com>
2024-01-26 23:55:39 -0300

    Add Filme class

--- src/ScreenMatch/Main.java
+++ src/ScreenMatch/Main.java
@@ -4,32 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Esse é o ScreenMatch");
+		Filme meuFilme = new Filme();
 		
-		String filme = "Interstellar";
+		meuFilme.nome = "Top Gun";
+		meuFilme.anoLancamento = 1990;
 		
-		int ano = 2014;
 		
-		double nota1 = 10;
-		
-		double nota2 = 9;
-		
-		double nota3 = 10;
-		
-		double notaMedia = (nota1 + nota2 + nota3) / 3;
-		
-		int classificacao = (int) (notaMedia/2);
-		
-		String mensagem = """
-				
-				Filme: %s
-				Ano: %d
-				Classificação: %d
-				
-				""".formatted(filme,ano,classificacao);
-		
-		System.out.println(mensagem);
-
+		meuFilme.exibirFichaTecnica();
 	}
 
 }
