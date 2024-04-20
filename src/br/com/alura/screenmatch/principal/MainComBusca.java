6c8cfd7039dd0ff7169548e504aa0f938a7c23b5
levysampaio <levysampaio@live.com>
2024-04-19 23:35:35 -0300

    add gson lb

--- src/br/com/alura/screenmatch/principal/MainComBusca.java
+++ src/br/com/alura/screenmatch/principal/MainComBusca.java
@@ -7,21 +7,37 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+
 public class MainComBusca {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
 		
-		
 		 HttpClient client = HttpClient.newHttpClient();
 		   HttpRequest request = HttpRequest.newBuilder()
-		         .uri(URI.create("http://www.omdbapi.com/?apikey=51a2a9de&t=tron"))
+		         .uri(URI.create("http://www.omdbapi.com/?apikey=51a2a9de&t=Avengers"))
 		         .build();
 		   
 		   HttpResponse<String> response = client
 				     .send(request, BodyHandlers.ofString());
 		   
-		   System.out.println(response.body());
+		   String json = response.body();
+		   
+		   System.out.println(json);
+		   
+		   Gson gson = new GsonBuilder()
+				   .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+				   .create();
+		   TituloOmdb meuTituloOmdb =  gson.fromJson(json, TituloOmdb.class);
+		   
+		   System.out.println(meuTituloOmdb);
+		   
+		   
 		
 	}
 }
